e6c8776e8e1d8480b39fa209dc38bd4a0fb067b5
turingou <o.u.turing@gmail.com>
2013-06-20 23:50:20 +0800

    修正配置项

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 exports.system = function(cb) {
-	fs.readFile(__dirname + '/package.json', function(err, data) {
+	fs.readFile('./package.json', function(err, data) {
 		if(err) throw err;
 		cb(JSON.parse(data));
 	});
